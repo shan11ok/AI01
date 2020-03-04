a100866edf026bd35ecfc65e43dd7456794e2853
myc <ranchia@163.com>
2020-03-04 17:20:21 +0800

    myc 更新代码，去掉将图片转换到指定长宽的代码。该部分代码会令图片失真，以致影响检测效果。

--- 6.2_车辆识别/car_recognition/yolo.py
+++ 6.2_车辆识别/car_recognition/yolo.py
@@ -24,9 +24,10 @@ class YOLO(object):
         "model_path": './model_data/yolo.h5',
         "anchors_path": './model_data/yolo_anchors.txt',
         "classes_path": './model_data/coco_classes.txt',
-        "score" : 0.3,
+        "score" : 0.5,
         "iou" : 0.45,
-        "model_image_size" : (416, 416),
+        #"model_image_size" : (416, 416),
+        "model_image_size" : (None, None),
         "gpu_num" : 1,
     }
 
